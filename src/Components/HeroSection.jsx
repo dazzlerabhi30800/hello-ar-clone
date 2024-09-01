6c68e033be507c4fc354850823678fb97b98cc37
abhishekchoudharywcs <abhishek.dev@webconsultancyservices.in>
2024-09-01 15:54:23 +0530

    added video on herosection & it's working fine.

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const HeroSection = () => {
-  return <header>HeroSection</header>;
+  return (
+    <header className="h-screen flex">
+      <video
+        loop
+        muted
+        style={{
+          cursor: "auto",
+
+          width: "100%",
+          height: "100%",
+          borderRadius: "0px",
+          display: "block",
+          objectFit: "cover",
+          backgroundColor: "rgba(0,0,0,0)",
+          objectPosition: "50% 50%",
+        }}
+        autoPlay
+      >
+        <source src="./hero-video.mp4" type="video/mp4" />
+      </video>
+    </header>
+  );
 };
 
 export default HeroSection;
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import HeroSection from '../components/Herosection'
+import React from "react";
+import HeroSection from "../components/Herosection";
 
 const Home = () => {
   return (
     <main>
       <HeroSection />
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
